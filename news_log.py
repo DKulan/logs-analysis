b749b072d08d31f430182fea19bf6bdccb353247
Daniel Kulangiev <danny.kulangiev@gmail.com>
2017-10-18 15:04:41 -0400

    Added output after views were generated

--- news_log.py
+++ news_log.py
@@ -1,15 +1,30 @@
+#!/usr/bin/python3
 import psycopg2
+import sys
+import time
 
 
 DB = "news"
 
 
+def connect(db_name):
+    """Connect to PostgreSQL database. Return connection"""
+    try:
+        db = psycopg2.connect("dbname={}".format(db_name))
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        print "Unable to connect to database"
+        sys.exit(1)
+
+
 def generate_views():
     """ This function generates three views in the database """
     # Connect to database
-    db = psycopg2.connect(database=DB)
-    c = db.cursor()
+    db, c = connect(DB)
 
+
+    # -- Question 1 --
     # Generate view to list the top articles
     c.execute("CREATE VIEW top_articles AS "
               "SELECT title, count(title) AS views "
@@ -18,7 +33,14 @@ def generate_views():
               "GROUP BY title "
               "ORDER BY views DESC "
               "LIMIT 3; ")
+    c.execute("SELECT * FROM top_articles;")
+    print("Most popular articles:")
+    for(title, views) in c.fetchall():
+        print("{}  -  {} views".format(title, views))
+    print("\n\n")
 
+
+    # -- Question 2 --
     # Generate view to list the top authors
     c.execute("CREATE VIEW top_authors AS "
               "SELECT name, count(title) AS views "
@@ -27,6 +49,12 @@ def generate_views():
               "AND log.path LIKE CONCAT('%', articles.slug) "
               "GROUP BY name "
               "ORDER BY views DESC; ")
+    c.execute("SELECT * FROM top_authors;")
+    print("Most popular authors:")
+    for(name, views) in c.fetchall():
+        print("{}  -  {} views".format(name, views))
+    print("\n\n")
+    
 
     # Generate view to list total requests for single day
     c.execute("CREATE VIEW requests AS "
@@ -34,6 +62,7 @@ def generate_views():
               "FROM log "
               "GROUP BY date "
               "ORDER BY req DESC;")
+    
 
     # Generate view to display requests that are not OK
     c.execute("CREATE VIEW err_requests AS "
@@ -42,6 +71,7 @@ def generate_views():
               "WHERE status != '200 OK' "
               "GROUP BY date "
               "ORDER BY req DESC;")
+    
 
     # Generate view to display percentage of errors for a given day
     c.execute("CREATE VIEW err_percent AS "
@@ -51,12 +81,20 @@ def generate_views():
               "WHERE err_requests.date = requests.date "
               "GROUP BY requests.date, err_requests.req, requests.req; ")
 
+    
+    # -- Question 3 --
     # Generate view to display the day with the most errors
     c.execute("CREATE VIEW most_errors AS "
-              "SELECT requests.date, err_percent.perc "
-              "FROM requests, err_percent, err_requests "
+              "SELECT * "
+              "FROM err_percent "
               "WHERE err_percent.perc > 1 "
-              "LIMIT 1; ")
+              "ORDER BY err_percent.perc DESC; ")
+    c.execute("SELECT * FROM most_errors;")
+    print("Days with > 1% of requests resulting in errors")
+    for(date, perc) in c.fetchall():
+        print("{}  -  {}% errors".format(date.strftime("%B %d, %Y"), perc))
+    print("\n\n")
+
 
     # Commit and close database
     db.commit()
